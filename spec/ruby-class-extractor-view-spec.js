8bb11521d5c369e297bc3f9b76ea450a26b45c41
Mateusz Sagan <mateusz@viaeurope.com>
2022-02-04 14:48:04 +0100

    First commit and functionality

---
+++ spec/ruby-class-extractor-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import RubyClassExtractorView from '../lib/ruby-class-extractor-view';
+
+describe('RubyClassExtractorView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
